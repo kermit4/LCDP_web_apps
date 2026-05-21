9f47db1f9a2a2cf402a1a9694c6a48d87bf70e43
Christopher Pearson <kermit4@gmail.com>
2026-05-21 15:25:48 +0100

    group_chat.html more reliable browser notifications, maybe

--- sw.js
+++ sw.js
@@ -5,3 +5,16 @@ self.addEventListener('message', e => {
         self.registration.showNotification(e.data.title, { body: e.data.body });
     }
 });
+
+self.addEventListener('notificationclick', e => {
+    e.notification.close();
+    e.waitUntil(
+        self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(list => {
+            for (const client of list) {
+                if (client.url.includes('/group_chat.html') && 'focus' in client)
+                    return client.focus();
+            }
+            return self.clients.openWindow('group_chat.html');
+        })
+    );
+});
